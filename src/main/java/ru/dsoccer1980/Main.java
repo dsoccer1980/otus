e08680346594037f0eacbd910099a88f8a84358d
dsoccer1980 <dsoccer1980@gmail.com>
2019-03-01 22:54:00 +0200

    HW2 add locale

--- src/main/java/ru/dsoccer1980/Main.java
+++ src/main/java/ru/dsoccer1980/Main.java
@@ -1,10 +1,12 @@
 package ru.dsoccer1980;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import ru.dsoccer1980.service.RunTesting;
 
 import java.io.*;
@@ -16,6 +18,15 @@ public class Main {
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource ms =
+                new ReloadableResourceBundleMessageSource();
+        ms.setBasename("bundle");
+        ms.setDefaultEncoding("UTF-8");
+        return ms;
     }
 
     public static void main(String[] args) throws IOException {
--- src/main/java/ru/dsoccer1980/service/RunTesting.java
+++ src/main/java/ru/dsoccer1980/service/RunTesting.java
@@ -1,16 +1,24 @@
 package ru.dsoccer1980.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Locale;
 
 @Service
 public class RunTesting {
 
     private TestingService testingService;
     private IOService ioService;
+    @Autowired
+    private MessageSource messageSource;
+    @Value("${locale.prop}")
+    private String locale;
 
     public RunTesting(TestingService testingService, IOService ioService) {
         this.testingService = testingService;
@@ -19,11 +27,18 @@ public class RunTesting {
 
     public void run() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        ioService.write("Write your name");
+        ioService.write(messageSource.getMessage(
+                "write.name",
+                null,
+                new Locale(locale)));
         String name = bufferedReader.readLine();
 
         testingService.showQuestions();
 
-        ioService.write("Result of " + name + ": " + testingService.getResult() + " right answers");
+        ioService.write(messageSource.getMessage(
+                "write.result",
+                new String[]{name, String.valueOf(testingService.getResult())},
+                new Locale(locale)));
+
     }
 }
